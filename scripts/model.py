02a634c66ea33c7c13b55870d53c5dc258b1afd2
arnegro <arneGro@online.de>
2025-06-06 09:29:07 +0200

    init

---
+++ scripts/model.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+class Model():
+    def __init__(self, P, G, mu, dt=1e-2, a0=None, u0=None):
+        self.P = P
+        self.G = G
+        self.mu = mu
+        self.dim = len(mu)
+        self.a0 = np.zeros(self.dim) if a0 is None else a0
+        self.u0 = np.zeros(self.dim) if u0 is None else u0
+        self.a = self.a0.copy()
+        self.u = self.u0.copy()
+        self.dt = dt
+        self._nrecvars = 2
+
+    def step_a(self, a, u, G, mu):
+        da = self.dt * (np.clip(u, a_min=mu, a_max=None)
+                      - G @ np.clip(a, a_min=0, a_max=None))
+        return a + da
+
+    def step_u(self, a, u, g, P):
+        du = self.dt * (g - P @ np.clip(a, a_min=0, a_max=None))
+        return u + du
+
+    def step(self, g):
+        self.a = self.step_a(self.a, self.u, self.G, self.mu)
+        self.u = self.step_u(self.a, self.u, g, self.P)
+        return self.a, self.u
+
+    def simulate(self, *args, **kwargs):
+        t, res = self._simulate(*args, _nrecvars=2, **kwargs)
+        a, u = res[:,0], res[:,1]
+        return t, a, u
+
+    def _simulate(self, g, T, a0=None, u0=None, verbose=True, _nrecvars=2):
+        self.a = a0 if a0 is not None else self.a0
+        self.u = u0 if u0 is not None else self.u0
+        t = np.arange(0, T, self.dt)
+        res = np.empty((len(t), _nrecvars, self.dim))
+        for i, _t in enumerate(t):
+            if verbose:
+                print(f'[{int(np.round(i/len(t)*100))}%] --- '
+                      f'{_t:.2f}/{T} hours simulated', end='       \r')
+            res[i] = self.step(g(_t))
+        if verbose: print()
+        return t, res
+
+class PiLearnModel(Model):
+    def __init__(self, *args, eta=1e-3, P_est=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.P_est = np.zeros((self.dim, self.dim)) \
+                                if P_est is None else P_est
+        self.eta = eta
+        self.u_est = self.u0.copy()
+
+    def step_P_est(self, a, u_est, u, g, P, P_est):
+        u2 = self.step_u(a, u, g, P)
+        u2_est = self.step_u(a, u_est, g, P_est)
+        u_dot = (u2 - u) / self.dt
+        u_dot_est = (u2_est - u_est) / self.dt
+        delta_u = u_dot - u_dot_est
+        # delta_u = u - u_est
+        # print(delta_u)
+        # print(a)
+        dP = - self.dt * np.outer(delta_u, np.clip(a, a_min=0, a_max=None)) * self.eta
+        # print(dP)
+        # print(P_est)
+        # input()
+        return P_est + dP
+
+    def step(self, g):
+        self.a = self.step_a(self.a, self.u_est, self.G, self.mu)
+        self.u = self.step_u(self.a, self.u, g, self.P)
+        self.u_est = self.step_u(self.a, self.u_est, g, self.P_est)
+        self.P_est = self.step_P_est(self.a, self.u_est, self.u, g,
+                                     self.P, self.P_est)
+        if hasattr(self, 'err'):
+            self.err.append(np.sum(self.P_est - self.P)**2)
+        return self.a, self.u, self.u_est
+
+    def simulate(self, *args, **kwargs):
+        self.err = []
+        t, res = self._simulate(*args, _nrecvars=3, **kwargs)
+        a, u, u_est = res[:,0], res[:,1], res[:,2]
+        return t, a, u, u_est
---
+++ scripts/simulate.py
@@ -0,0 +1,43 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from model import Model, PiLearnModel
+
+if __name__ == '__main__':
+    n = 3
+    P = np.zeros((n, n))
+    P[0,0] = 4
+    P[1,1] = 4
+    P[2,:] = [-1, -1, 3]
+    G = np.zeros((n, n))
+    G[0,:] = [2, 3, 3]
+    G[1,:] = [3, 4, 3]
+    G[2,:] = [3, 3, 2]
+    G *= 4
+    mu = np.array([0, 20, 0])
+    g = lambda t : np.array([4, 0, 0])
+
+    P_est = P + np.random.randn(*P.shape)
+    model = PiLearnModel(P, G, mu, P_est=P_est)
+    print(model.P)
+    print(np.round(model.P_est, decimals=3))
+    t, a, u, u_est = model.simulate(g, T=300)
+    print(np.round(model.P_est, decimals=3))
+    # model = Model(P, G, mu)
+    # t, a, u = model.simulate(g, T=100)
+
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex=True)
+    ax1.plot(t, np.clip(a, a_min=0, a_max=None))
+    # ax2.plot(t, np.clip(u, a_min=None, a_max=None))
+    ax2.plot(t, u)
+    ax3.plot(t, np.clip(u_est, a_min=mu, a_max=None),
+             label=['HH', 'leisure', 'work'])
+    ax4.plot(t, model.err)
+    ax1.set(ylabel=r'$\vec{a}^{> 0}$')
+    # ax2.set(ylabel=r'$\vec{u}^{> \vec{\mu}}$')
+    ax2.set(ylabel=r'$\vec{u}$')
+    y0, y1 = ax3.get_ylim()
+    ax3.set(xlabel='time', ylabel=r'$\vec{u}_{est}^{> \vec{\mu}}$',
+            ylim=(y0-.1*(y1-y0), y1))
+    ax3.legend(ncols=3, frameon=False)
+    plt.tight_layout()
+    plt.show()
